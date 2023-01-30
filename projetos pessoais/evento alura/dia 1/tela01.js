db01144d9b746b228ca90902e0f07c9055045b60
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-01-30 14:37:49 -0300

    Update tela01.js

--- projetos pessoais/evento alura/dia 1/tela01.js
+++ projetos pessoais/evento alura/dia 1/tela01.js
@@ -1,3 +1,5 @@
-var ValorEmDolar = 30; /*CamelCase = colocar a inicial de toda palavra em maiuscula, usado no javascript.*/
-var ValorEmReal
-alert(valoremdolar) /*quando se referir a uma variavel, não colocar entre aspas.*/
+var ValorEmDolar = 30; /*CamelCase = usado para separar palavras, colocando a inicial de toda palavra em maiuscula, usado no javascript.*/
+var ValorEmReal = 5.32; /*sempre usar ponto ao inves de virgula certo = (5.40) errado = (5,40)*/
+var Conta = ValorEmDolar*ValorEmReal
+
+alert('R$' + Conta.toFixed([2])) /*quando se referir a uma variavel, não colocar entre aspas. Num.toFixed([NumeroDeCasas]) = definir um numero de casas para o numero. Para adicionar coisas como o R$ no codigo usando o "+".*/
