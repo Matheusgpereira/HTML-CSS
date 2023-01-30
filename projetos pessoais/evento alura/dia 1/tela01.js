e33c8ed8378ac7a82ee68ae9e14c376c90bc0fa7
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-01-30 14:14:16 -0300

    atualização tela01

---
+++ projetos pessoais/evento alura/dia 1/tela01.js
@@ -0,0 +1,3 @@
+var ValorEmDolar = 30; /*CamelCase = colocar a inicial de toda palavra em maiuscula, usado no javascript.*/
+var ValorEmReal
+alert(valoremdolar) /*quando se referir a uma variavel, não colocar entre aspas.*/
