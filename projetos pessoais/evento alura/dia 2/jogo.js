2a65416dcb43c159a2d76f1cb30277dafdb94a12
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-01-31 16:55:35 -0300

    Update jogo.js

--- projetos pessoais/evento alura/dia 2/jogo.js
+++ projetos pessoais/evento alura/dia 2/jogo.js
@@ -1,16 +1,13 @@
-var numSecreto = 581;
+var numSecreto = parseInt(Math.random() * 1000); //paseInt() = considere inteiro Math.random() = selecionar um numero aleatorio
 var chute = prompt('digite seu chute') //primpt('') = alert com caixa para digitar
 
-while (chute != numSecreto) {
-    chute
-}
-
+while (chute != numSecreto) {       //while(caracteristica){execução} = enquanto a caracteristica definida não caontecer executar algo
+    var chute = prompt('digite seu chute')
 if (chute == numSecreto) {
     alert('correto')
-}
-if (chute < numSecreto) {
+} else if (chute < numSecreto) {
     alert('Baixo de mais')
-}
-if (chute > numSecreto) {
+} else if (chute > numSecreto) {
     alert('Alto de mais')
+}
 }
