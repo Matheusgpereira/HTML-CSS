0e714c34d79a691ba83576d690c9e74f98fcb6b4
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-01-31 16:41:55 -0300

    update jogo.js

---
+++ projetos pessoais/evento alura/dia 2/jogo.js
@@ -0,0 +1,16 @@
+var numSecreto = 581;
+var chute = prompt('digite seu chute') //primpt('') = alert com caixa para digitar
+
+while (chute != numSecreto) {
+    chute
+}
+
+if (chute == numSecreto) {
+    alert('correto')
+}
+if (chute < numSecreto) {
+    alert('Baixo de mais')
+}
+if (chute > numSecreto) {
+    alert('Alto de mais')
+}
