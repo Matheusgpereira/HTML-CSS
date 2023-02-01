47389877ab3166c345956751fab656ccaa53192b
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-02-01 20:03:42 -0300

    update aluraflix

---
+++ projetos pessoais/evento alura/dia 3/aluraflix.js
@@ -0,0 +1,5 @@
+var filme = 'https://upload.wikimedia.org/wikipedia/pt/d/dc/Notting_Hill.jpg';
+var serie = 'https://br.web.img3.acsta.net/pictures/14/01/23/13/30/209537.jpg';
+var animação  = 'https://upload.wikimedia.org/wikipedia/pt/3/3f/Treasure_Planet_%28Disney%29.jpg';
+
+document.write(); //maineira de escrever no documento html usando javascript.
