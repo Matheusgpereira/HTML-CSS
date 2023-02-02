84f489bf86477843554dc868b8698bb3f212f4bd
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-02-02 15:50:00 -0300

    Update filmesFavoritos.js

---
+++ projetos pessoais/evento alura/dia 4/filmesFavoritos.js
@@ -0,0 +1,4 @@
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById('filme').value //.getElementById = função de pegar um documento pelo id, .value = pegar o valor do id
+    alert(filmeFavorito);
+}
