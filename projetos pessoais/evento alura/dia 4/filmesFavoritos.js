4e116bba31e0162ec65a2548a0e679b8797caebb
Matheus guerreiro <121069321+Matheusgpereira@users.noreply.github.com>
2023-02-02 16:13:58 -0300

    Update filmesFavoritos.js

--- projetos pessoais/evento alura/dia 4/filmesFavoritos.js
+++ projetos pessoais/evento alura/dia 4/filmesFavoritos.js
@@ -1,4 +1,7 @@
 function adicionarFilme() {
-    var filmeFavorito = document.getElementById('filme').value //.getElementById = função de pegar um documento pelo id, .value = pegar o valor do id
-    alert(filmeFavorito);
+    var filmeFavorito = document.getElementById('filme').value // .value = pegar o valor do id.
+    var elementoListaFilmes = document.getElementById('listaFilmes') //.getElementById = função de pegar um documento pelo id. 
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>'
+    document.getElementById('filme').value = ''
 }
+//https://s2.glbimg.com/1SEyI21pWu8q-3ydMB6kqMMGELs=/0x0:2000x1202/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/u/Y/7rgbnyR8AizUbcIO4wQw/hp-capa.jpg
